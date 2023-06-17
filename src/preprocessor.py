fc0419e101bb8d468ab7d9c92bc9200574e5339d
Samvel Karapetyan <130899451+SamvelKarapetyaan@users.noreply.github.com>
2023-06-17 13:58:27 -0700

    Delete preprocessor.py

--- src/preprocessor.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-import tensorflow as tf
-
-class Preprocessor:
-    def __init__(self):
-        pass
-
-    def __call__(self, path):
-        pass # get tf.keras.datasets and return
